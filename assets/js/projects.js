b02b520e63ad4e5cef6fb34890503830c723a9fb
IsaacCruz11 <zack115010@gmail.com>
2024-05-03 11:26:28 -0600

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -8,7 +8,7 @@ projects.p1 = {
     'roles'       : '',
     'tools'       : 'Wordpress, CSS3 y Figma',
     'enddate'     : '23/04/2024',
-    'description' : 'La asesoría para este sitio web consistió en una guía detallada sobre las funcionalidades de WordPress a través de un recorrido completo del sitio y una exposición de ejemplos asociados con el objetivo de brindar una noción más amplia de esta herramienta y aprovechar al máximo sus capacidades.Así como brindarles una perspectiva profesional sobre los cambios en apariencia y funcionalidad de su plataforma, atendiendo a sus necesidades particulares como organización sin fines de lucro.'
+    'description' : ''
 };
 projects.p2 = {
     'image'       : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
@@ -18,7 +18,7 @@ projects.p2 = {
     'roles'       : 'Asesor para el uso de Wordpress',
     'tools'       : 'Wordpress, CSS3 y Figma',
     'enddate'     : '23/04/2024',
-    'description' : ''
+    'description' : 'La asesoría para este sitio web consistió en una guía detallada sobre las funcionalidades de WordPress a través de un recorrido completo del sitio y una exposición de ejemplos asociados con el objetivo de brindar una noción más amplia de esta herramienta y aprovechar al máximo sus capacidades.Así como brindarles una perspectiva profesional sobre los cambios en apariencia y funcionalidad de su plataforma, atendiendo a sus necesidades particulares como organización sin fines de lucro.'
 };
 projects.p3 = {
     'image'       : 'bg-[url("/assets/img/projects/edusuperior/edu-superior-index-01.png")]',
@@ -28,27 +28,27 @@ projects.p3 = {
     'roles'       : 'Desarrollador Wordpress y Maquetador',
     'tools'       : 'Wordpress, HTML5, CSS3, Javascript y MySql',
     'enddate'     : '22/04/2024',
-    'description' : ''
+    'description' : 'Para este sitio web se utilizaron las herramientas de WordPress, HTML5 y CSS3 para una maquetación 100% fiel a un nuevo diseño y de esta manera garantizar una óptima transición y adaptabilidad para una experiencia de usuario coherente, dinámica e instintiva.'
 };
 projects.p4 = {
     'image'       : 'bg-[url("/assets/img/projects/dtevivienda/DTE-index.jpeg")]',
-    'title'       : 'Recepción de Documentos Electrónicos Tributarios',
+    'title'       : 'Recepción de DTE',
     'client'      : 'Ministerio de Vivienda de El Salvador',
     'url'         : 'N/A',
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
     'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
     'enddate'     : '22/03/2024',
-    'description' : ''
+    'description' : 'El sistema de Recepción de Documentos Tributarios Electónicos (DTE) se encarga de leer los correos que contienen las facturas / documentos tributarios electrónicos y de esta manera extraer información que concierne a la contabilidad del ministerio. Como Analista, el trabajo consistió en la definición de requisitos y procesos, la creación de una interfaz con experiencia de usuario intuitiva y atractiva utilizando la herramienta de Figma. Los roles desempeñados para este proyecto fueron como analista, desarrollador y SCRUM Máster de manera que se utilizaron metodologías ágiles de manera efectiva.'
 };
 projects.p5 = {
     'image'       : 'bg-[url("/assets/img/projects/usrvivienda/Gestion-Usuarios-index.jpeg")]',
-    'title'       : 'Gestión de Usuarios para Múltiples Sistemas',
+    'title'       : 'Gestión de Usuarios - Múltiples Sistemas',
     'client'      : 'Ministerio de Vivienda de El Salvador',
     'url'         : 'N/A',
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
     'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
     'enddate'     : '01/12/2023',
-    'description' : ''
+    'description' : 'El objetivo de este sistema es centralizar a los usuarios de todos los sistemas del ministerio para poder hacer cambios de contraseña, de roles, de permisos y también poder crear usuarios nuevos. Los roles desempeñados para este proyecto fueron como analista, diseñador UX/IU y SCRUM Máster de manera que se definieron los requisitos y procesos para centralizar a todos los usuarios de todos los sistemas del ministerio, se creó una interfaz intuitiva y atractiva para facilitar la gestión de usuarios y se implementaron soluciones para realizar cambios de contraseña, roles, permisos y la creación de usuarios nuevos a través de metodologías ágiles que facilitaran la comunicación y colaboración dentro del equipo de desarrollo.'
 };
 projects.p6 = {
     'image'       : 'bg-[url("/assets/img/projects/proveeduria/Proveeduria-index.jpeg")]',
@@ -58,11 +58,11 @@ projects.p6 = {
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
     'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
     'enddate'     : '24/04/2024',
-    'description' : ''
+    'description' : 'Proveeduría es un sistema que se encarga de recibir las requisiciones de insumos creadas por los usuarios internos, se agregaron los modulos: productos, proveedores, categorías, bodegas, compras, entre otros. con el fin de tener un inventario preciso y un flujo de peticiones y despacho más ágil. Para finalizar se agregaron reportes de contabilidad, de existencias de requisiciones y de liquidación'
 };
 projects.p7 = {
     'image'       : 'bg-[url("/assets/img/projects/assessment/Assessment-login.png")]',
-    'title'       : 'Assessment - E-learning y evaluación del personal',
+    'title'       : 'Assessment - E-learning',
     'client'      : 'Rena Ware Internacional',
     'url'         : 'N/A',
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
@@ -72,7 +72,7 @@ projects.p7 = {
 };
 projects.p8 = {
     'image'       : 'bg-[url("/assets/img/projects/renakit/renakit-index-01.png")]',
-    'title'       : 'RenaRecursos - Gestión de documentos internos',
+    'title'       : 'RenaRecursos - Gestión de documentos',
     'client'      : 'Rena Ware Internacional',
     'url'         : 'N/A',
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
@@ -82,7 +82,7 @@ projects.p8 = {
 };
 projects.p9 = {
     'image'       : 'bg-[url("/assets/img/projects/ccsa/ccsa-index.png")]',
-    'title'       : 'Sistema de Notas - Ingreso de notas para bachillerato',
+    'title'       : 'Sistema de Notas - Ingreso de notas',
     'client'      : 'Centro Cultural Salvadoreño Americano',
     'url'         : 'N/A',
     'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
