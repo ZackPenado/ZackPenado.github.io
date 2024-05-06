c8238ef028ff81751bf73ad141ebabd973d6bada
IsaacCruz11 <zack115010@gmail.com>
2024-05-06 14:57:19 -0600

    Updates

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,94 +1,103 @@
 let projects = new Object();
 
 projects.p1 = {
-    'image'       : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
-    'title'       : 'Sitio web Libélula',
-    'client'      : 'Libélula Spanish School',
-    'url'         : 'http://libelulaschool.com/',
-    'roles'       : '',
-    'tools'       : 'Wordpress, CSS3 y Figma',
-    'enddate'     : '23/04/2024',
-    'description' : ''
+    'image'          : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
+    'title'          : 'Sitio web Libélula',
+    'client'         : 'Libélula Spanish School',
+    'url'            : 'http://libelulaschool.com/',
+    'roles'          : '',
+    'tools'          : 'Wordpress, CSS3 y Figma',
+    'enddate'        : '23/04/2024',
+    'description_es' : '',
+    'description_en' : ''
 };
 projects.p2 = {
-    'image'       : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
-    'title'       : 'Sitio web CoCoDA',
-    'client'      : 'CoCoDA',
-    'url'         : 'https://cocoda.org',
-    'roles'       : 'Asesor para el uso de Wordpress',
-    'tools'       : 'Wordpress, CSS3 y Figma',
-    'enddate'     : '23/04/2024',
-    'description' : 'La asesoría para este sitio web consistió en una guía detallada sobre las funcionalidades de WordPress a través de un recorrido completo del sitio y una exposición de ejemplos asociados con el objetivo de brindar una noción más amplia de esta herramienta y aprovechar al máximo sus capacidades.Así como brindarles una perspectiva profesional sobre los cambios en apariencia y funcionalidad de su plataforma, atendiendo a sus necesidades particulares como organización sin fines de lucro.'
+    'image'          : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
+    'title'          : 'Sitio web CoCoDA',
+    'client'         : 'CoCoDA',
+    'url'            : 'https://cocoda.org',
+    'roles'          : 'Asesor para el uso de Wordpress',
+    'tools'          : 'Wordpress, CSS3 y Figma',
+    'enddate'        : '23/04/2024',
+    'description_es' : 'La asesoría para este sitio web consistió en una guía detallada sobre las funcionalidades de WordPress a través de un recorrido completo del sitio y una exposición de ejemplos asociados con el objetivo de brindar una noción más amplia de esta herramienta y aprovechar al máximo sus capacidades. Así como brindarles una perspectiva profesional sobre los cambios en apariencia y funcionalidad de su plataforma, atendiendo a sus necesidades particulares como organización sin fines de lucro. ',
+    'description_en' : 'The consultancy for this website consisted of a detailed guide to the functionalities of WordPress through a complete tour of the site and an exhibition of associated examples with the aim of providing a broader notion of this tool and making the most of its capabilities. As well as providing them with a professional perspective on the changes in appearance and functionality of their platform, addressing their particular needs as a non-profit organization.'
 };
 projects.p3 = {
-    'image'       : 'bg-[url("/assets/img/projects/edusuperior/edu-superior-index-01.png")]',
-    'title'       : 'Sitio web Edu Superior',
-    'client'      : 'Edu Superior',
-    'url'         : 'https://edusuperior.org.sv',
-    'roles'       : 'Desarrollador Wordpress y Maquetador',
-    'tools'       : 'Wordpress, HTML5, CSS3, Javascript y MySql',
-    'enddate'     : '22/04/2024',
-    'description' : 'Para este sitio web se utilizaron las herramientas de WordPress, HTML5 y CSS3 para una maquetación 100% fiel a un nuevo diseño y de esta manera garantizar una óptima transición y adaptabilidad para una experiencia de usuario coherente, dinámica e instintiva.'
+    'image'          : 'bg-[url("/assets/img/projects/edusuperior/edu-superior-index-01.png")]',
+    'title'          : 'Sitio web Edu Superior',
+    'client'         : 'Edu Superior',
+    'url'            : 'https://edusuperior.org.sv',
+    'roles'          : 'Desarrollador Wordpress y Maquetador',
+    'tools'          : 'Wordpress, HTML5, CSS3, Javascript y MySql',
+    'enddate'        : '22/04/2024',
+    'description_es' : 'Para este sitio web se utilizaron las herramientas de WordPress, HTML5 y CSS3 para una maquetación 100% fiel a un nuevo diseño y de esta manera garantizar una óptima transición y adaptabilidad para una experiencia de usuario coherente, dinámica e intuitiva.',
+    'description_en' : 'For this website, WordPress, HTML5 and CSS3 tools were used for a layout that was a 100% faithful to a new design in order to guarantee an optimal transition and adaptability for a coherent, dynamic and instinctive user experience.'
 };
 projects.p4 = {
-    'image'       : 'bg-[url("/assets/img/projects/dtevivienda/DTE-index.jpeg")]',
-    'title'       : 'Recepción de DTE',
-    'client'      : 'Ministerio de Vivienda de El Salvador',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '22/03/2024',
-    'description' : 'El sistema de Recepción de Documentos Tributarios Electónicos (DTE) se encarga de leer los correos que contienen las facturas / documentos tributarios electrónicos y de esta manera extraer información que concierne a la contabilidad del ministerio. Como Analista, el trabajo consistió en la definición de requisitos y procesos, la creación de una interfaz con experiencia de usuario intuitiva y atractiva utilizando la herramienta de Figma. Los roles desempeñados para este proyecto fueron como analista, desarrollador y SCRUM Máster de manera que se utilizaron metodologías ágiles de manera efectiva.'
+    'image'          : 'bg-[url("/assets/img/projects/dtevivienda/DTE-index.jpeg")]',
+    'title'          : 'Recepción de DTE',
+    'client'         : 'Ministerio de Vivienda de El Salvador',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '22/03/2024',
+    'description_es' : 'Este sistema se encarga de leer los correos que contienen las facturas / documentos tributarios electrónicos y de esta manera extraer información que concierne a la contabilidad del ministerio. Como Analista, el trabajo consistió en la definición de requisitos y procesos, la creación de una interfaz con experiencia de usuario intuitiva y atractiva utilizando la herramienta de Figma. Los roles desempeñados para este proyecto fueron como analista, desarrollador y SCRUM Máster de manera que se utilizaron metodologías ágiles de manera efectiva.',
+    'description_en' : 'This system is responsible for reading the emails containing the invoices/electronic tax documents in order to extract information that concerns the Ministry´s accounting. As an Analyst, the work consisted of defining requirements and processes, creating an interface with an intuitive and attractive user experience using the Figma tool. The played roles for this project were as an analyst, developer and SCRUM Master so that agile methodologies were used effectively.'
 };
 projects.p5 = {
-    'image'       : 'bg-[url("/assets/img/projects/usrvivienda/Gestion-Usuarios-index.jpeg")]',
-    'title'       : 'Gestión de Usuarios - Múltiples Sistemas',
-    'client'      : 'Ministerio de Vivienda de El Salvador',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '01/12/2023',
-    'description' : 'El objetivo de este sistema es centralizar a los usuarios de todos los sistemas del ministerio para poder hacer cambios de contraseña, de roles, de permisos y también poder crear usuarios nuevos. Los roles desempeñados para este proyecto fueron como analista, diseñador UX/IU y SCRUM Máster de manera que se definieron los requisitos y procesos para centralizar a todos los usuarios de todos los sistemas del ministerio, se creó una interfaz intuitiva y atractiva para facilitar la gestión de usuarios y se implementaron soluciones para realizar cambios de contraseña, roles, permisos y la creación de usuarios nuevos a través de metodologías ágiles que facilitaran la comunicación y colaboración dentro del equipo de desarrollo.'
+    'image'          : 'bg-[url("/assets/img/projects/usrvivienda/Gestion-Usuarios-index.jpeg")]',
+    'title'          : 'Gestión de Usuarios - Múltiples Sistemas',
+    'client'         : 'Ministerio de Vivienda de El Salvador',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '01/12/2023',
+    'description_es' : 'El objetivo de este sistema es centralizar a los usuarios de todos los sistemas del ministerio para poder hacer cambios de contraseña, de roles, de permisos y también poder crear usuarios nuevos. Los roles desempeñados para este proyecto fueron como analista, diseñador UX/IU y SCRUM Máster de manera que se definieron los requisitos necesarios para lograr el objetivo del sistema, se creó una interfaz intuitiva y atractiva para facilitar la gestión de usuarios y se implementaron soluciones para realizar cambios de contraseña, roles, permisos y la creación de usuarios nuevos a través de metodologías ágiles que facilitaran la comunicación y colaboración dentro del equipo de desarrollo.',
+    'description_en' : 'The objective of this system is to centralize the users of all the ministry´s systems to be able to change passwords, roles, permissions and also be able to create new users. The played roles for this project were as an analyst, UX/UI designer and SCRUM Master so that the requirements and processes were defined to centralize all users of all the ministry´s systems, an intuitive and attractive interface was created to facilitate management of users. Solutions were also implemented to make password changes, roles, permissions and the creation of new users through agile methodologies that facilitate communication and collaboration within the development team.'
 };
 projects.p6 = {
-    'image'       : 'bg-[url("/assets/img/projects/proveeduria/Proveeduria-index.jpeg")]',
-    'title'       : 'Proveeduría - Gestión de Insumos',
-    'client'      : 'Ministerio de Vivienda de El Salvador',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '24/04/2024',
-    'description' : 'Proveeduría es un sistema que se encarga de recibir las requisiciones de insumos creadas por los usuarios internos, se agregaron los modulos: productos, proveedores, categorías, bodegas, compras, entre otros. con el fin de tener un inventario preciso y un flujo de peticiones y despacho más ágil. Para finalizar se agregaron reportes de contabilidad, de existencias de requisiciones y de liquidación'
+    'image'          : 'bg-[url("/assets/img/projects/proveeduria/Proveeduria-index.jpeg")]',
+    'title'          : 'Proveeduría - Gestión de Insumos',
+    'client'         : 'Ministerio de Vivienda de El Salvador',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '24/04/2024',
+    'description_es' : 'Proveeduría es un sistema que se encarga de recibir las requisiciones de insumos creadas por los usuarios internos, se agregaron los modulos: productos, proveedores, categorías, bodegas, compras, entre otros. con el fin de tener un inventario preciso y un flujo de peticiones y despacho más ágil. Para finalizar se agregaron reportes de contabilidad, de existencias de requisiciones y de liquidación',
+    'description_en' : 'Proveeduría is a system that is responsible for receiving input requests created by internal users, the modules were added: products, suppliers, categories, warehouses, purchases, among others, in order to have an accurate inventory and a flow of requests and more agile dispatch. Finally, accounting, requisition stock and liquidation reports were added.'
 };
 projects.p7 = {
-    'image'       : 'bg-[url("/assets/img/projects/assessment/Assessment-login.png")]',
-    'title'       : 'Assessment - E-learning',
-    'client'      : 'Rena Ware Internacional',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '24/04/2024',
-    'description' : ''
+    'image'          : 'bg-[url("/assets/img/projects/assessment/Assessment-login.png")]',
+    'title'          : 'Assessment - E-learning',
+    'client'         : 'Rena Ware Internacional',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '24/04/2024',
+    'description_es' : 'Assessment es un sistema de E-learning en Rena Ware internacional que se desarrolló a la medida con el fin de poder capacitar al personal de la fuerza de ventas, lo conforman diferentes categorías donde el agente de ventas puede ir escalando y así poder alcanzar un grado de reclutador o capacitador, los cursos tienen videos, lecturas y exámenes que al ser aprobados se genera un diploma automáticamente y se envía por correo al agente de ventas.',
+    'description_en' : 'Assessment is an international E-learning system in Rena Ware that was custom developed in order to train sales force personnel, it is made up of different categories where the sales agent can climb and thus be able to reach a degree. as a recruiter or trainer, the courses have videos, readings and exams that, upon passing, a diploma was automatically generated and sent by mail to the sales agent.'
 };
 projects.p8 = {
-    'image'       : 'bg-[url("/assets/img/projects/renakit/renakit-index-01.png")]',
-    'title'       : 'RenaRecursos - Gestión de documentos',
-    'client'      : 'Rena Ware Internacional',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '24/04/2024',
-    'description' : ''
+    'image'          : 'bg-[url("/assets/img/projects/renakit/renakit-index-01.png")]',
+    'title'          : 'RenaRecursos - Gestión de documentos',
+    'client'         : 'Rena Ware Internacional',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '24/04/2024',
+    'description_es' : 'Renarecursos en un sistema de Rena Ware internacional al cual se le hizo un rediseño y actualización de versión de framework, pasando del Laravel 6 a Laravel 9. El sistema gestiona documentos internos para la fuerza de ventas y administrativos. Son catálogos para mostrar a los clientes y documentos para mejorar las habilidades de venta.',
+    'description_en' : 'Renare resources in an international Rena Ware system that underwent a redesign and update of the framework version, going from Laravel 6 to Laravel 9. The system manages internal documents for the sales force and administration. They are catalogs to show to customers and documents to improve sales skills.'
 };
 projects.p9 = {
-    'image'       : 'bg-[url("/assets/img/projects/ccsa/ccsa-index.png")]',
-    'title'       : 'Sistema de Notas - Ingreso de notas',
-    'client'      : 'Centro Cultural Salvadoreño Americano',
-    'url'         : 'N/A',
-    'roles'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'       : 'Laravel, Livewire, Tailwind y Alpine JS',
-    'enddate'     : '24/04/2024',
-    'description' : ''
+    'image'          : 'bg-[url("/assets/img/projects/ccsa/ccsa-index.png")]',
+    'title'          : 'Sistema de Notas - Ingreso de notas',
+    'client'         : 'Centro Cultural Salvadoreño Americano',
+    'url'            : 'N/A',
+    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'enddate'        : '24/04/2024',
+    'description_es' : 'Sistema de notas tiene como objetivo almacenar las notas de los estudiantes de bachillerato del Centro cultural salvadoreño americano, a cada profesor se le asignan grados cada inicio de año y en las fechas requeridas el sistema habilita el ingreso de notas de actividades y exámenes. El reporte de notas o cuadro de notas se genera al finalizar un trimestre y es entregado a los padres en una reunión presencial.',
+    'description_en' : 'The purpose of the grading system is to store the grades of the high school students of the Salvadoran American Cultural Center. Each teacher is assigned grades at the beginning of the year and on the required dates the system enables the entry of grades from activities and exams. The grade report or grade table is generated at the end of a quarter and is delivered to parents in an in-person meeting.'
 };
 
 let projectsCards = "";
@@ -108,7 +117,7 @@ for (const [key, value] of Object.entries(projects)) {
     projectsCards += "    <p><b>Roles:</b> " + value.roles +"</p>";
     projectsCards += "    <p><b>Herramientas:</b> " + value.tools +"</p>";
     projectsCards += "    <p><b>Entrega del proyecto:</b> " + value.enddate +"</p>";
-    projectsCards += "    <p class='text-justify'><b>Descripción:</b><br> " + value.description +"</p>";
+    projectsCards += "    <p class='text-justify'><b>Descripción:</b><br> " + value.description_es +"</p>";
     projectsCards += "  </div>";
     projectsCards += "</div>";
 }
