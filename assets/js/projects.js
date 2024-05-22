242e408472a04c730701b121bc8c5d42dfb42258
IsaacCruz11 <zack115010@gmail.com>
2024-05-21 21:33:54 -0600

    Changes 20240521

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,17 +1,18 @@
 let projects = new Object();
 
 projects.p1 = {
-    'image'          : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
+    'image'          : 'bg-[url("/assets/img/projects/libelula/libelula-index.png")]',
     'title_es'       : 'Sitio web Libélula',
     'title_en'       : 'Libélula Website',
     'client_es'      : 'Escuela de Español Libélula',
     'client_en'      : 'Libélula Spanish School',
     'url'            : 'http://libelulaschool.com/',
-    'roles'          : '',
-    'tools'          : 'HTML5, Tailwind CSS, Javascript y Figma',
-    'enddate'        : '23/04/2024',
-    'description_es' : '',
-    'description_en' : ''
+    'roles_es'       : 'Diseñador, Maquetador y Desarrollador',
+    'roles_en'       : 'Designer, Layout Designer and Developer',
+    'tools'          : 'HTML5 | Tailwind CSS | PHP | Javascript | Figma | MySql',
+    'enddate'        : '22/05/2024',
+    'description_es' : 'Para este sitio web se realizó un desarrollo a la medida trabajando desde el enfoque Mobile first logrando así un responsive perfecto. Además, se utilizó la herramienta Alpine JS para crear un efecto reactivo e interactivo con los usuarios.',
+    'description_en' : 'For this website, a custom development was carried out working from the Mobile first approach, thus achieving perfect responsiveness. Additionally, the Alpine JS tool was used to create a reactive and interactive effect with users.'
 };
 projects.p2 = {
     'image'          : 'bg-[url("/assets/img/projects/cocoda/CoCoDA-index-01.png")]',
@@ -20,8 +21,9 @@ projects.p2 = {
     'client_es'      : 'CoCoDA',
     'client_en'      : 'CoCoDA',
     'url'            : 'https://cocoda.org',
-    'roles'          : 'Asesor para el uso de Wordpress',
-    'tools'          : 'Wordpress, CSS3 y Figma',
+    'roles_es'       : 'Asesor',
+    'roles_en'       : 'Consultant',
+    'tools'          : 'Wordpress | CSS3 | Figma',
     'enddate'        : '23/04/2024',
     'description_es' : 'La asesoría para este sitio web consistió en una guía detallada sobre las funcionalidades de WordPress a través de un recorrido completo del sitio y una exposición de ejemplos asociados con el objetivo de brindar una noción más amplia de esta herramienta y aprovechar al máximo sus capacidades. Así como brindarles una perspectiva profesional sobre los cambios en apariencia y funcionalidad de su plataforma, atendiendo a sus necesidades particulares como organización sin fines de lucro. ',
     'description_en' : 'The consultancy for this website consisted of a detailed guide to the functionalities of WordPress through a complete tour of the site and an exhibition of associated examples with the aim of providing a broader notion of this tool and making the most of its capabilities. As well as providing them with a professional perspective on the changes in appearance and functionality of their platform, addressing their particular needs as a non-profit organization.'
@@ -33,8 +35,9 @@ projects.p3 = {
     'client_es'      : 'Edu Superior',
     'client_en'      : 'Edu Superior',
     'url'            : 'https://edusuperior.org.sv',
-    'roles'          : 'Desarrollador Wordpress y Maquetador',
-    'tools'          : 'Wordpress, HTML5, CSS3, Javascript y MySql',
+    'roles_es'       : 'Desarrollador y Maquetador',
+    'roles_en'       : 'Developer and Layout Designer',
+    'tools'          : 'Wordpress | HTML5 | CSS3 | Javascript | MySql',
     'enddate'        : '22/04/2024',
     'description_es' : 'Para este sitio web se utilizaron las herramientas de WordPress, HTML5 y CSS3 para una maquetación 100% fiel a un nuevo diseño y de esta manera garantizar una óptima transición y adaptabilidad para una experiencia de usuario coherente, dinámica e intuitiva.',
     'description_en' : 'For this website, WordPress, HTML5 and CSS3 tools were used for a layout that was a 100% faithful to a new design in order to guarantee an optimal transition and adaptability for a coherent, dynamic and instinctive user experience.'
@@ -46,8 +49,9 @@ projects.p4 = {
     'client_es'      : 'Ministerio de Vivienda de El Salvador',
     'client_en'      : 'Ministry of Housing of El Salvador',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'roles_en'       : 'Analyst, UX/UI Designer, Developer and SCRUM Master',
+    'tools'          : 'Laravel | Livewire | Tailwind | Alpine JS | Sql Server',
     'enddate'        : '22/03/2024',
     'description_es' : 'Este sistema se encarga de leer los correos que contienen las facturas / documentos tributarios electrónicos y de esta manera extraer información que concierne a la contabilidad del ministerio. Como Analista, el trabajo consistió en la definición de requisitos y procesos, la creación de una interfaz con experiencia de usuario intuitiva y atractiva utilizando la herramienta de Figma. Los roles desempeñados para este proyecto fueron como analista, desarrollador y SCRUM Máster de manera que se utilizaron metodologías ágiles de manera efectiva.',
     'description_en' : 'This system is responsible for reading the emails containing the invoices/electronic tax documents in order to extract information that concerns the Ministry´s accounting. As an Analyst, the work consisted of defining requirements and processes, creating an interface with an intuitive and attractive user experience using the Figma tool. The played roles for this project were as an analyst, developer and SCRUM Master so that agile methodologies were used effectively.'
@@ -59,8 +63,9 @@ projects.p5 = {
     'client_es'      : 'Ministerio de Vivienda de El Salvador',
     'client_en'      : 'Ministry of Housing of El Salvador',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'roles_en'       : 'Analyst, UX/UI Designer, Developer and SCRUM Master',
+    'tools'          : 'Laravel | Livewire | Tailwind | Alpine JS | Sql Server',
     'enddate'        : '01/12/2023',
     'description_es' : 'El objetivo de este sistema es centralizar a los usuarios de todos los sistemas del ministerio para poder hacer cambios de contraseña, de roles, de permisos y también poder crear usuarios nuevos. Los roles desempeñados para este proyecto fueron como analista, diseñador UX/IU y SCRUM Máster de manera que se definieron los requisitos necesarios para lograr el objetivo del sistema, se creó una interfaz intuitiva y atractiva para facilitar la gestión de usuarios y se implementaron soluciones para realizar cambios de contraseña, roles, permisos y la creación de usuarios nuevos a través de metodologías ágiles que facilitaran la comunicación y colaboración dentro del equipo de desarrollo.',
     'description_en' : 'The objective of this system is to centralize the users of all the ministry´s systems to be able to change passwords, roles, permissions and also be able to create new users. The played roles for this project were as an analyst, UX/UI designer and SCRUM Master so that the requirements and processes were defined to centralize all users of all the ministry´s systems, an intuitive and attractive interface was created to facilitate management of users. Solutions were also implemented to make password changes, roles, permissions and the creation of new users through agile methodologies that facilitate communication and collaboration within the development team.'
@@ -72,8 +77,9 @@ projects.p6 = {
     'client_es'      : 'Ministerio de Vivienda de El Salvador',
     'client_en'      : 'Ministry of Housing of El Salvador',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
+    'roles_en'       : 'Analyst, UX/UI Designer, Developer and SCRUM Master',
+    'tools'          : 'Laravel | Livewire | Tailwind | Alpine JS | Sql Server',
     'enddate'        : '24/04/2024',
     'description_es' : 'Proveeduría es un sistema que se encarga de recibir las requisiciones de insumos creadas por los usuarios internos, se agregaron los modulos: productos, proveedores, categorías, bodegas, compras, entre otros. con el fin de tener un inventario preciso y un flujo de peticiones y despacho más ágil. Para finalizar se agregaron reportes de contabilidad, de existencias de requisiciones y de liquidación',
     'description_en' : 'Proveeduría is a system that is responsible for receiving input requests created by internal users, the modules were added: products, suppliers, categories, warehouses, purchases, among others, in order to have an accurate inventory and a flow of requests and more agile dispatch. Finally, accounting, requisition stock and liquidation reports were added.'
@@ -85,8 +91,9 @@ projects.p7 = {
     'client_es'      : 'Rena Ware Internacional',
     'client_en'      : 'Rena Ware International',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista y Desarrollador',
+    'roles_en'       : 'Analyst and Developer',
+    'tools'          : 'Laravel | Bootstrap 5 | Javascript | MySql',
     'enddate'        : '24/04/2024',
     'description_es' : 'Assessment es un sistema de E-learning en Rena Ware internacional que se desarrolló a la medida con el fin de poder capacitar al personal de la fuerza de ventas, lo conforman diferentes categorías donde el agente de ventas puede ir escalando y así poder alcanzar un grado de reclutador o capacitador, los cursos tienen videos, lecturas y exámenes que al ser aprobados se genera un diploma automáticamente y se envía por correo al agente de ventas.',
     'description_en' : 'Assessment is an international E-learning system in Rena Ware that was custom developed in order to train sales force personnel, it is made up of different categories where the sales agent can climb and thus be able to reach a degree. as a recruiter or trainer, the courses have videos, readings and exams that, upon passing, a diploma was automatically generated and sent by mail to the sales agent.'
@@ -98,8 +105,9 @@ projects.p8 = {
     'client_es'      : 'Rena Ware Internacional',
     'client_en'      : 'Rena Ware International',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista y Desarrollador',
+    'roles_en'       : 'Analyst and Developer',
+    'tools'          : 'Laravel | Bootstrap 5 | Javascript | MySql',
     'enddate'        : '24/04/2024',
     'description_es' : 'Renarecursos en un sistema de Rena Ware internacional al cual se le hizo un rediseño y actualización de versión de framework, pasando del Laravel 6 a Laravel 9. El sistema gestiona documentos internos para la fuerza de ventas y administrativos. Son catálogos para mostrar a los clientes y documentos para mejorar las habilidades de venta.',
     'description_en' : 'Renare resources in an international Rena Ware system that underwent a redesign and update of the framework version, going from Laravel 6 to Laravel 9. The system manages internal documents for the sales force and administration. They are catalogs to show to customers and documents to improve sales skills.'
@@ -111,8 +119,9 @@ projects.p9 = {
     'client_es'      : 'Centro Cultural Salvadoreño Americano',
     'client_en'      : 'Salvadoran American Cultural Center',
     'url'            : 'N/A',
-    'roles'          : 'Analista, Diseñador UX/UI, Desarrollador y SCRUM Master',
-    'tools'          : 'Laravel, Livewire, Tailwind y Alpine JS',
+    'roles_es'       : 'Analista y Desarrollador',
+    'roles_en'       : 'Analyst and Developer',
+    'tools'          : 'HTML | PHP | CSS | MySql',
     'enddate'        : '24/04/2024',
     'description_es' : 'Sistema de notas tiene como objetivo almacenar las notas de los estudiantes de bachillerato del Centro cultural salvadoreño americano, a cada profesor se le asignan grados cada inicio de año y en las fechas requeridas el sistema habilita el ingreso de notas de actividades y exámenes. El reporte de notas o cuadro de notas se genera al finalizar un trimestre y es entregado a los padres en una reunión presencial.',
     'description_en' : 'The purpose of the grading system is to store the grades of the high school students of the Salvadoran American Cultural Center. Each teacher is assigned grades at the beginning of the year and on the required dates the system enables the entry of grades from activities and exams. The grade report or grade table is generated at the end of a quarter and is delivered to parents in an in-person meeting.'
@@ -132,9 +141,9 @@ for (const [key, value] of Object.entries(projects)) {
     projectsCards += "    <p class='text-xl font-bold'>"+ value.title_es +"</p>";
     projectsCards += "    <p><b>Cliente:</b> " + value.client_es +"</p>";
     projectsCards += "    <p><b>URL:</b> <a href='" + value.url +"' target='_blank'>" + value.url +"</a></p>";
-    projectsCards += "    <p><b>Roles:</b> " + value.roles +"</p>";
+    projectsCards += "    <p><b>Roles:</b> " + value.roles_es +"</p>";
     projectsCards += "    <p><b>Herramientas:</b> " + value.tools +"</p>";
-    projectsCards += "    <p><b>Entrega del proyecto:</b> " + value.enddate +"</p>";
+    //projectsCards += "    <p><b>Entrega del proyecto:</b> " + value.enddate +"</p>";
     projectsCards += "    <p class='text-justify'><b>Descripción:</b><br> " + value.description_es +"</p>";
     projectsCards += "  </div>";
     projectsCards += "</div>";
@@ -154,9 +163,9 @@ for (const [key, value] of Object.entries(projects)) {
     projectsCardsEN += "    <p class='text-xl font-bold'>"+ value.title_en +"</p>";
     projectsCardsEN += "    <p><b>Client:</b> " + value.client_en +"</p>";
     projectsCardsEN += "    <p><b>URL:</b> <a href='" + value.url +"' target='_blank'>" + value.url +"</a></p>";
-    projectsCardsEN += "    <p><b>Roles:</b> " + value.roles +"</p>";
+    projectsCardsEN += "    <p><b>Roles:</b> " + value.roles_en +"</p>";
     projectsCardsEN += "    <p><b>Tools:</b> " + value.tools +"</p>";
-    projectsCardsEN += "    <p><b>Project end date:</b> " + value.enddate +"</p>";
+    //projectsCardsEN += "    <p><b>Project end date:</b> " + value.enddate +"</p>";
     projectsCardsEN += "    <p class='text-justify'><b>Description:</b><br> " + value.description_en +"</p>";
     projectsCardsEN += "  </div>";
     projectsCardsEN += "</div>";
